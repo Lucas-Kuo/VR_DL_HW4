a5931bb46176e66da245a250881a73afa4d1e393
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-12-21 22:57:37 +0800

    Create config.py

---
+++ model/config.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+import os
+
+# specify root path to the BSDS500 dataset
+ROOT_PATH = "dataset/"
+
+# specify paths to the different splits of the dataset
+TRAIN_SET = os.path.join(ROOT_PATH, "train")
+VAL_SET = os.path.join(ROOT_PATH, "val")
+TEST_SET = os.path.join(ROOT_PATH, "test")
+
+# specify the initial size of the images and downsampling factor
+ORIG_SIZE = (300, 300)
+DOWN_FACTOR = 3
+
+# specify number of RDB blocks, batch size, number of epochs, and
+# initial learning rate to train our model
+RDB_LAYERS = 3
+BATCH_SIZE = 8
+EPOCHS = 100
+LR = 1e-3
+
+#define paths to serialize trained model, training history plot, and
+# path to our inference visualizations
+SUPER_RES_MODEL = os.path.join("output", "super_res_model")
+TRAINING_PLOT = os.path.join("output", "training.png")
+VISUALIZATION_PATH = os.path.join("output", "visualizations")
